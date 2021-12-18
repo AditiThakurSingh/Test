8d2767e8900d90b9dbaadf7bb7d0fd0ca31657cd
Aditi Thakur <aditislabthakur@gmail.com>
2021-12-18 18:49:10 +0530

    eventlistner

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+
+for(i=0;i<document.querySelector(".drum").length;i++){
+
+document.querySelectorAll("button")[i].addEventListener(click,response)
+}
+function response(){
+    alert("I got clicked")
+}
